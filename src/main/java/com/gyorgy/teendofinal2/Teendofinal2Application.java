e6a579f101752b9d1a6a7a235464c8efac6a1ddb
wizardon1000mgcaffeine <stumpf.gyorgy0519@gmail.com>
2025-08-26 02:55:37 +0200

    elso

---
+++ src/main/java/com/gyorgy/teendofinal2/Teendofinal2Application.java
@@ -0,0 +1,13 @@
+package com.gyorgy.teendofinal2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Teendofinal2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Teendofinal2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gyorgy/teendofinal2/Teendofinal2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyorgy.teendofinal2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Teendofinal2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
